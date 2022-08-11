384cdf89f2789fe133b28f165617d1649d274fd2
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 19:44:52 +0200

    test

---
+++ Untitled-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		int x;
+		cin >> x;
+
+		if (x%5 == 0)
+		{
+			cout << " multiple of 5";
+		}
+		else
+		{
+			cout << "not multiple of 5";
+		}
+
+	}
