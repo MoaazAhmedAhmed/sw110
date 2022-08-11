ba395fbd47afc009bd8d632b8db59bd72d94e9ba
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 19:39:53 +0200

    t

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 int main()
 {
-	double x;
-	double y;
-	cin >> x >> y;
+		int x;
+		cin >> x;
 
-    if (x > y)
-	{
-		cout << x;
-	}else 
-	{
-		cout << y;
+		if (x%5 == 0)
+		{
+			cout << " multiple of 5";
+		}
+		else
+		{
+			cout << "not multiple of 5";
+		}
+
 	}
-
-}
-
-
