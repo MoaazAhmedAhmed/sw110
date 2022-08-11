a6fdfd3ae732f20f639cf146664ec73222cc99c7
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 19:31:42 +0200

    tes

---
+++ Untitled-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		char x ;
+		cin >> x;
+		switch (x)
+		{
+		case'b':
+			cout << "this character is b";
+			break;
+
+		case 'B':
+		  cout << "this character is B";
+			break;
+		default:
+			cout << "not b";
+		}
+	
+		
+	}
+
