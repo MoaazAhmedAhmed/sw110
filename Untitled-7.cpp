f5234913545e53ec0d960e20783eaf916c75cc58
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 20:36:23 +0200

    test

---
+++ Untitled-7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+char printgrade(double score)
+{
+	if (score >= 90)
+	{
+		cout << "A";
+	}
+	else if (x >= 80)
+	{
+		cout << "B";
+	}
+	else if (x >= 70)
+	{
+		cout << "C";
+	}
+	else if (x >= 60)
+	{
+		cout << "C";
+	}
+	else
+		cout << "F",
+	
+	return grade;
+}
+int main()
+{
+	cout << printgrade(90)
+		return 0;
+}
