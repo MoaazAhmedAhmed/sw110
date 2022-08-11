429de709edcb9ad0a679084ce9e89b8188b399cf
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 22:56:45 +0200

    test

---
+++ Untitled-52.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void printHello();
+int main()
+{
+    printHello();
+    cout<<endl;
+    return 0;
+}
+void printHello()
+{
+    cout<<"Hello, World!";
+}
