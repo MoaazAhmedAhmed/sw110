eff930cc7fbd671e54fe10805408de900ce3e69b
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 22:44:44 +0200

    test

---
+++ Untitled-38.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	intx;
+	cout << "enter number :" << endl;
+	cin >> x;
+	if ( x == 2||x== 3||x== 5||x== 7||x== 11||x== 13||x== 17||x== 19||x== 23||x== 29)
+	{
+		cout << "prime number " << endl;
+	}
+	else
+	{
+		cout << "not prime number " << endl;
+	}
+	return 0;
+}
