be3bcfcd107248226f99d7fc04a4089e32f06992
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 22:43:55 +0200

    test

---
+++ Untitled-37.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int input;
+	cout << "enter input :" << endl;
+	cin >> input;
+	if (input == 1 || -1)
+	{
+		cout << "OUTPUT: 1" << endl;
+	}
+	return 0;
+}
