b9209921ab2072d0e1f6cd77f697fb11e8a919c4
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 20:36:56 +0200

    test

---
+++ Untitled-8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void myfunction(string name1, string name2)
+{
+	cout << "greetings" << name1 << endl;
+	cout << "greetings" << name2 << endl;
+	return name;
+}
+int main()
+{
+	int x, y;
+	cout << "enter first nmae : " << endl;
+	cin >> x;
+	cout << "enter second name : " << endl;
+	cin >> y;
+	cout << x << " " << y << endl;
+}
