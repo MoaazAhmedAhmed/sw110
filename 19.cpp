012a1fd35ac0b0d69e9a4329c41c0a45b7df7de9
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 21:06:57 +0200

    test

---
+++ 19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		int x;
+		cin >> x;
+
+		if (x<=5||x>15)
+		{
+			cout << " accepted ";
+		}
+		else
+		{
+			cout << "not accepted";
+		}
+
+	}
