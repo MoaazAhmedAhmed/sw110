1387eb8388470a93eae4c80965b1e050e76be73e
MoaazAhmedAhmed <moaaz2107540@miuegypt.edu.eg>
2022-08-11 22:40:35 +0200

    test

---
+++ Untitled-30cpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num,i;
+	cout << "enter number :";
+	cin>> num;
+	for (i = 1; i <= 10; i++)
+	{
+		cout << num << "x" << i << " = " << num*i << endl;
+}
